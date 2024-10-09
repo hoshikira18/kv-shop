3aa614837f2bf00afbebc32eb2a2a5af7c8be17c
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-10-10 01:58:41 +0700

    feat: JDBC connection

---
+++ src/java/models/UserDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author khuye
+ */
+public class UserDAO extends MyDAO {
+    
+}
