809e05109f843613435cc8315a2c0f8bbfd7d781
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-10-13 16:09:59 +0700

    add: create new category

---
+++ src/java/controller/admin/categories/CreateServlet.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.admin.categories;
+
+import controller.CategoriesControler;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import models.Category;
+
+/**
+ *
+ * @author khuye
+ */
+@WebServlet(urlPatterns = {"/admin/categories/create"})
+public class CreateServlet extends HttpServlet {
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        String categoryName = (String) req.getParameter("category-name");
+        CategoriesControler cc = new CategoriesControler();
+        cc.insertCategory(new Category(categoryName));
+        resp.sendRedirect("/shop/admin/categories");
+    }
+    
+}
